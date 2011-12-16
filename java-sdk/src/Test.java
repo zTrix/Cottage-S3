de73da5ccb91c489f142400f64f040e8713ee9b0
zTrix <i@ztrix.me>
2011-12-16 19:56:39 +0800

    add login

--- java-sdk/src/Test.java
+++ java-sdk/src/Test.java
@@ -6,12 +6,20 @@ public class Test {
 
     public static void main(String []args) {
         Toolkit.setServer("127.0.0.1");
-        int rs = Toolkit.ping();
+        int rs;
+
+        rs = Toolkit.ping();
         if (rs != 0) {
             Zlog.e("ping server failed: retcode = " + rs);
         } else {
             Zlog.i("ping server OK!");
         }
-        Toolkit.login("test@test.com", "password");
+
+        rs = Toolkit.login("test@test.com", "password");
+        if (rs != 0) {
+            Zlog.e("login failed, please see more specific info in stdout");
+        } else {
+            Zlog.i("login success");
+        }
     }
 }
--- java-sdk/src/org/cs3/Toolkit.java
+++ java-sdk/src/org/cs3/Toolkit.java
@@ -1,10 +1,12 @@
 package org.cs3;
 
 import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLEncoder;
 
 import org.json.JSONObject;
 
@@ -43,8 +45,37 @@ public class Toolkit {
         return ret;
     }
 
-    public static boolean login(String username, String password) {
-        return false;
+    public static int login(String email, String password) {
+        if (baseurl == null) {
+            return -1000;
+        }
+        int ret = 0;
+        try {
+            URL url = new URL(getApiUrl("login"));
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("POST");
+            conn.setDoInput(true);
+            conn.setDoOutput(true);
+            DataOutputStream printout = new DataOutputStream(conn.getOutputStream ());
+            String content = "email=" + URLEncoder.encode (email) + "&password=" + URLEncoder.encode(password);
+            printout.writeBytes (content);
+            printout.flush ();
+            printout.close ();
+
+            JSONObject rs = getJson(conn.getInputStream());
+            ret = rs.getInt(ERR);
+            if (ret != 0) {
+                Zlog.e(rs.getString(MSG));
+            }
+        } catch (Exception e) {
+            ret = -1001;
+            Zlog.e(e);
+        }
+        return ret;
+    }
+
+    private static String getApiUrl(String api) {
+        return baseurl + "api/" + api;
     }
 
     private static JSONObject getJson(InputStream in) {
